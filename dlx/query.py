9b90724c1ef2ce7e7479e277e1f6236bb7dfbccb
Joseph Bukhari <Joseph.Bukhari@UNHQ.UN.ORG>
2019-03-28 18:08:45 -0400

    begin documentation

--- dlx/query.py
+++ dlx/query.py
@@ -1,6 +1,8 @@
-'''
+"""
+Functions for building pymongo queries. 
 
-'''
+All functions return BSON objects suitable for use in pymongo queries. The returned objects can be embedded in dicts in order to compose more complex queries.
+"""
 
 import re
 from bson import SON
@@ -10,21 +12,49 @@ from .config import Configs
 # JMARC queries
 
 def match_value(tag,code,val):
+	"""Builds a query document for matching single subfield values. 
+	
+	parameters
+	----------
+	param1 : str
+		The field tag to match.
+	param2 : str / None
+		The subfield code to match. Use None as the code if matching a controlfield value.
+	param3 : str / Pattern 
+		Value to match against. Exact string or a compiled Pattern.
+	
+	returns
+	-------
+		BSON 
+	
+	examples
+	-------
+	>>> query = match_value('269', 'a', '1999')
+	>>> first_result = dlx.JBIB.find_one(query)
+	
+	>>> complex_query = {
+		'$and' : [
+			match_value('008', None, re.compile('1999')),
+			match_value('245', 'a', re.compile('United Nations')),
+		]
+	}
+	"""
+	
 	valtype = val.__class__.__name__
 	
-	auth_tag = auth_controlled(tag,code)
+	auth_tag = __auth_controlled(tag,code)
 	
 	if auth_tag is not None:
-		xrefs = get_xrefs(auth_tag,code,val)
+		xrefs = __get_xrefs(auth_tag,code,val)
 		
 		return in_xrefs(tag,code,*xrefs)
 
 	if valtype == 'str':
-		return exact_value(tag,code,val)
+		return __exact_value(tag,code,val)
 	elif valtype == 'Pattern':
-		return re_value(tag,code,val)
+		return __re_value(tag,code,val)
 		
-def exact_value(tag,code,val):
+def __exact_value(tag,code,val):
 	return SON (
 		data = {
 			tag + '.subfields' : {
@@ -34,7 +64,7 @@ def exact_value(tag,code,val):
 		}
 	)
 	
-def re_value(tag,code,re):
+def __re_value(tag,code,re):
 	return SON (
 		data = {
 			tag + '.subfields' : {
@@ -57,6 +87,26 @@ def exact_xref(tag,code,xref):
 	)
 
 def in_xrefs(tag,code,*xrefs):
+	"""Builds a query document for matching single subfield xrefs against a list of xrefs. 
+	
+	parameters
+	----------
+		param1 : str
+			The field Tag to match.
+		param2 : str
+			The subfield code to match.
+		*args : int
+			Xrefs (authority IDs) to match against.
+			
+	returns
+	-------
+		BSON
+		
+	examples
+	--------
+		>>> query = in_xrefs('650','a',268584,274431)
+		>>> dlx.JBIB.find(query)
+	"""
 	return SON (
 		data = {
 			tag + '.subfields' : {
@@ -70,7 +120,7 @@ def in_xrefs(tag,code,*xrefs):
 		}
 	)
 	
-def get_xrefs(tag,code,val):
+def __get_xrefs(tag,code,val):
 	cur = DB.auths.find(match_value(tag,code,val),{'_id':1})
 	
 	ret_vals = []
@@ -80,7 +130,7 @@ def get_xrefs(tag,code,val):
 		
 	return ret_vals
 	
-def auth_controlled(tag,code):
+def __auth_controlled(tag,code):
 	try:
 		return Configs.authority_controlled[tag][code]
 	except:
