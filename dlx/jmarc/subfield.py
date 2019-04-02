63199bf0c2831e585a3861a3ebe6d3cd33b35ba8
j. bukhari <JB@jb-air.local>
2019-04-02 18:04:41 -0400

    remove value() property

--- dlx/jmarc/subfield.py
+++ dlx/jmarc/subfield.py
@@ -1,29 +1,24 @@
 '''
 '''
 
+from bson import SON
+
 class Subfield(object):
 	def __init__(self):
 		pass
-		
-	def to_bson(self):
-		dict = self.__dict__
-		bson = SON()
-		
-		bson['code'] = self.code
-		
-		if self.__class__.__name__ == 'Literal':
-			bson['value'] = self.value
-		elif self.__class__.__name__ == 'Linked':
-			bson['xref'] = self.xref
-			
-		return bson
-
+	
 class Literal(Subfield):
 	def __init__(self,code,value):
 		self.code = code
 		self.value = value
+		
+	def to_bson(self):
+		return SON(data = {'code' : self.code, 'value' : self.value})
 	
 class Linked(Subfield):	
 	def __init__(self,code,xref):
 		self.code = code
 		self.xref = int(xref)
+		
+	def to_bson(self):
+		return SON(data = {'code' : self.xref, 'xref' : self.xref})
