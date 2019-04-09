005315598c738b12af5184f456eb0a807aae83ee
j. bukhari <JB@jb-air.local>
2019-04-09 13:44:52 -0400

    implement to_mrc; bugfixes

--- dlx/jmarc/record.py
+++ dlx/jmarc/record.py
@@ -7,6 +7,7 @@ from dlx.db import DB
 from dlx.query import jmarc as Q
 from .subfield import Literal, Linked
 from .field import Controlfield, Datafield
+
 		
 class JMARC(object):
 	_cache = {}
@@ -19,15 +20,47 @@ class JMARC(object):
 			return JMARC._cache[xref][code]
 		except:
 			auth = JAUTH.match_id(xref)
-			value = auth.header_value(code)
 			
+			if auth is None:
+				value = 'N/A'
+			else:	
+				value = auth.header_value(code)
+				
 			if xref not in JMARC._cache.keys():
 				JMARC._cache[xref] = {}
 				
 			JMARC._cache[xref][code] = value
 				
 			return value
+			
+	@staticmethod
+	def serialize_subfield(sub):
+		if sub.__class__.__name__ == 'Linked':
+			return {sub.code : JMARC.lookup(sub.xref,sub.code)}
+		else:
+			return {sub.code : sub.value}
+			
+	@staticmethod
+	def field_text(f):
+		delim = u'\u001f'
+		field_terminator = u'\u001e'
+		text = ''
+
+		if f.__class__.__name__ == 'Controlfield':
+			text = f.value
+		else:
+			text += f.ind1 + f.ind2
+			
+			for sub in (f.subfields):
+				if hasattr(sub,'value'):
+					text += delim + sub.code + sub.value
+				else:
+					text += delim + sub.code + JMARC.lookup(sub.xref,sub.code)
 		
+		text += field_terminator
+		
+		return text
+
 	## class
 		
 	#### database query handlers
@@ -68,8 +101,8 @@ class JMARC(object):
 	def match_value(cls,tag,code,val):
 		cursor = cls.handle().find(Q.match_value(tag,code,val))
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 	
 	@classmethod
 	@check_connection
@@ -81,16 +114,16 @@ class JMARC(object):
 	def match_values(cls,*tuples):
 		cursor = cls.handle().find(Q.and_values(*tuples))
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 			
 	@classmethod	
 	@check_connection
 	def match_values_or(cls,*tuples):
 		cursor = cls.handle().find(Q.or_values(*tuples))
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 	
 	@classmethod	
 	@check_connection
@@ -102,8 +135,8 @@ class JMARC(object):
 	def match_field(cls,tag,*tuples):
 		cursor = cls.handle().find(Q.match_field(tag,*tuples))
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 			
 	@classmethod
 	@check_connection	
@@ -115,21 +148,24 @@ class JMARC(object):
 	def match_xrefs(cls,tag,*xrefs):
 		cursor = cls.handle().find(Q.match_xrefs(tag,*xrefs))
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 	
 	@classmethod
 	@check_connection
 	def find(cls,doc):
 		cursor = cls.handle().find(doc)
 		
-		for dict in cursor:
-			yield cls(dict)
+		for doc in cursor:
+			yield cls(doc)
 	
 	@classmethod
 	@check_connection
 	def find_one(cls,doc):
-		return cls(cls.handle().find_one(doc))
+		found = cls.handle().find_one(doc)
+		
+		if found is not None:
+			return cls(found)
 		
 	#### database index creation
 	
@@ -161,23 +197,25 @@ class JMARC(object):
 	
 	## instance 
 	
-	def __init__(self,dict={}):
+	def __init__(self,doc={}):
 		self.controlfields = []
 		self.datafields = []
 		
-		if '_id' in dict.keys():
-			self.id = str(dict['_id'])
+		if doc is None: doc = {}
 		
-		for tag in filter(lambda x: False if x == '_id' else True, dict.keys()):
+		if '_id' in doc.keys():
+			self.id = str(doc['_id'])
+		
+		for tag in filter(lambda x: False if x == '_id' else True, doc.keys()):
 			
 			if tag == '000':
-				self.leader = dict['000'][0]
+				self.leader = doc['000'][0]
 				
 			if tag[:2] == '00':
-				for value in dict[tag]:
+				for value in doc[tag]:
 					self.controlfields.append(Controlfield(tag,value))
 			else:
-				for field in dict[tag]:
+				for field in doc[tag]:
 					ind1 = field['indicators'][0]
 					ind2 = field['indicators'][1]
 					subfields = []
@@ -195,7 +233,7 @@ class JMARC(object):
 			return self.controlfields + self.datafields
 			
 		return filter(lambda x: True if x.tag in tags else False, self.controlfields + self.datafields)
-							
+			
 	def get_field(self,tag):
 		return next(self.get_fields(tag), None)
 			
@@ -237,6 +275,7 @@ class JMARC(object):
 	
 	def to_bson(self):
 		bson = SON()
+		bson['_id'] = int(self.id)
 		
 		for tag in self.tags():
 			bson[tag] = [field.to_bson() for field in self.get_fields(tag)]
@@ -261,7 +300,7 @@ class JMARC(object):
 			fields.append(
 				{
 					f.tag : {
-						'subfields' : list(map(lambda x: {x.code : x.value}, f.subfields)),
+						'subfields' : [JMARC.serialize_subfield(sub) for sub in f.subfields],
 						'ind1' : f.ind1,
 						'ind2' : f.ind2,
 					}
@@ -272,9 +311,45 @@ class JMARC(object):
 		
 		return json.dumps(mij)
 		
+	def to_mrc(self):
+		directory = ''
+		data = ''
+		next_start = 0
+		field_terminator = u'\u001e'
+		record_terminator = u'\u001d'
+		
+		for f in filter(lambda x: x.tag != '000', self.get_fields()):
+			text = JMARC.field_text(f)
+			data += text
+			field_length = len(text.encode('utf-8'))
+			directory += f.tag + str(field_length).zfill(4) + str(next_start).zfill(5)
+			next_start += field_length
+			
+		directory += field_terminator
+		data += record_terminator
+		leader_dir_len = len(directory.encode('utf-8')) + 24
+		base_address = str(leader_dir_len).zfill(5)
+		total_len = str(leader_dir_len + len(data.encode('utf-8'))).zfill(5)
+		
+		new_leader = total_len \
+			+ self.leader[5:9] \
+			+ 'a' \
+			+ self.leader[10:11] \
+			+ base_address \
+			+ self.leader[17:21] \
+			+ '4500'
+
+		return new_leader + directory + data
+	
+	def to_mrk(self):
+		pass	
+		
+	def to_xml(self):
+		pass
+				
 class JBIB(JMARC):
-	def __init__(self,dict={}):
-		super().__init__(dict)
+	def __init__(self,doc={}):
+		super().__init__(doc)
 		
 	#### shorctuts
 	
@@ -312,15 +387,16 @@ class JBIB(JMARC):
 			return DB.files.find_one(file_by_symbol_lang(symbol,lang))['uri']
 		except:
 			return ''
-
-				
+			
 class JAUTH(JMARC):
-	def __init__(self,dict={}):
-		super().__init__(dict)
+	def __init__(self,doc={}):
+		super().__init__(doc)
 		
-		self.header = next(filter(lambda field: field.tag[0:1] == '1', self.get_fields()))
+		self.header = next(filter(lambda field: field.tag[0:1] == '1', self.get_fields()), None)
 		
 	def header_value(self,code):
+		if self.header is None:
+			return
+			
 		for sub in filter(lambda sub: sub.code == code, self.header.subfields):
 			return sub.value
-
