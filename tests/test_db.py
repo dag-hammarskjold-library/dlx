884c756ca4aca8db60cece795939232e662a5015
j. bukhari <JB@jb-air.home>
2019-04-10 07:16:00 -0400

    update

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,6 @@
+"""
+Tests for dlx.DB
+"""
 
 from unittest import TestCase
 import mongomock 
@@ -6,11 +9,12 @@ from dlx import DB
 class Test(TestCase):
 	def test_connect(self):
 		db = DB.connect('mongodb://u:pw@dummy.com/?authSource=dummy',mock=True)
-		
-		self.assertIsInstance(db, mongomock.Database)
-		
+		self.assertIsInstance(db,mongomock.Database)
 		self.assertTrue(DB.check_connection())
-	
-
-	
 		
+	def test_db(self):
+		self.assertIsInstance(DB.handle,mongomock.Database)
+		self.assertIsInstance(DB.bibs,mongomock.Collection)
+		self.assertIsInstance(DB.auths,mongomock.Collection)
+		self.assertIsInstance(DB.files,mongomock.Collection)
+		self.assertIsInstance(DB.config,dict)
