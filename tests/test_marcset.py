9be8b02afd804721076e46ff33692e282046b7b6
jbukhari <joe.bukhari@gmail.com>
2020-02-18 13:47:19 -0500

    Set (#77)
    * use pytest; iterator on MarcSet
    
    * cleanup

--- tests/test_marcset.py
+++ tests/test_marcset.py
@@ -1,221 +1,121 @@
-import re, os
-from unittest import TestCase
+import pytest, os, inspect
 
-from dlx import DB, marc
-from dlx.marc import Marc, Bib, Auth, BibSet, AuthSet
-from dlx.marc import QueryDocument, Condition
+# Fixtures are loaded automatically py pytest from ./conftest.py
+# Tests run in order.
+# Remember to prefix test function names with "test_".
+# Database state is maintained globally. Use the `db` fixture to reset the test database.
 
-### test data
-
-class Data(object):
-    jbib = {
-        '_id' : 999,
-        '000' : ['leader'],
-        '008' : ['controlfield'],
-        '245' : [
-            {
-                'indicators' : [' ',' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'This'
-                    },
-                    {
-                        'code' : 'b',
-                        'value' : 'is the'
-                    },
-                    {
-                        'code' : 'c',
-                        'value' : 'title'
-                    }
-                ]
-            }
-        ],
-        '520' : [
-            {
-                'indicators' : [' ' ,' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'description'
-                    }
-                ]
-            },
-            {
-                'indicators' : [' ' ,' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'another description'
-                    },
-                    {
-                        'code' : 'a',
-                        'value' : 'repeated subfield'
-                    }
-                ]
-            }
-        ],
-        '650' : [
-            {
-                'indicators' : [' ', ' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'xref' : 777
-                    }
-                ],
-            }
-        ],
-        '710' : [
-            {
-                'indicators' : [' ',' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'xref' : 333
-                    }
-                ]
-            }
-        ]
-    }
+def test_mocked():
+    from dlx import DB
+    from mongomock import MongoClient as Mocked
     
-    jbib2 = {
-        '_id' : 555,
-        '000' : ['leader'],
-        '245' : [
-            {
-                'indicators' : [' ',' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'Another'
-                    },
-                    {
-                        'code' : 'b',
-                        'value' : 'is the'
-                    },
-                    {
-                        'code' : 'c',
-                        'value' : 'title'
-                    }
-                ]
-            }
-        ],
-        '650' : [
-            {
-                'indicators' : [' ' ,' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'xref' : 777
-                    }
-                ]
-            }
-        ]
-    }
+    assert isinstance(DB.client, Mocked)
     
-    jauth = {
-        '_id' : 777,
-        '150' : [
-            {
-                'indicators' : [' ', ' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'header text'
-                    }
-                ]
-            }
-        ]
-    }
+def test_init(bibs, auths):
+    from dlx.marc import BibSet, Bib, AuthSet, Auth
     
-    jauth2 = {
-        '_id' : 333,
-        '150' : [
-            {
-                'indicators' : [' ', ' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'another header'
-                    }
-                ]
-            }
-        ]
-    }
+    records = [Bib(x) for x in bibs]
+    bibset = BibSet(records)
+    assert isinstance(bibset, BibSet)
+    assert len(bibset.records) == 2
+    assert bibset.count == 2
     
-    invalid = {
-        '_id' : 'string invalid',
-        '150' : [
-            {
-                'indicators' : [' ', ' '],
-                'subfields' : [
-                    {
-                        'code' : 'a',
-                        'value' : 'header text'
-                    }
-                ]
-            }
-        ]
-    }    
+    records = [Auth(x) for x in auths]
+    authset = AuthSet(records)
+    assert isinstance(authset, AuthSet)
+    assert len(authset.records) == 2
+    assert authset.count == 2
+    
+def test_iterate(db):
+    from dlx.marc import Bib, BibSet, Auth, AuthSet
+    
+    for bib in BibSet.from_query({}):
+        assert isinstance(bib, Bib)
+        
+    for auth in AuthSet.from_query({}):
+        assert isinstance(auth, Auth)
 
-### tests  
+def test_from_query(db):
+    from dlx.marc import MarcSet, BibSet, AuthSet, QueryDocument, Condition
     
-class Batch(TestCase):
-    def setUp(self):
-        DB.connect('mongodb://.../?authSource=dummy',mock=True)
-        Bib(Data.jbib).commit()
-        Bib(Data.jbib2).commit()
+    bibset = BibSet.from_query({'_id': {'$in': [1, 2]}})
+    assert isinstance(bibset, (MarcSet, BibSet))
+    assert bibset.count == 2
+    assert isinstance(bibset.records, map)
+    bibset.cache()
+    assert isinstance(bibset.records, list)
+    
+    bibset = BibSet.from_query({}, skip=0, limit=1)
+    assert bibset.count == 1
+    for bib in bibset:
+        assert bib.id == 1
+    assert len(list(bibset.records)) == 0
+    assert bibset.count == 1
+    
+    conditions = [
+        Condition(tag='150', subfields={'a': 'Header'}),
+        Condition(tag='200', modifier='not_exists')
+    ]
+    authset = AuthSet.from_query(conditions)
+    assert isinstance(authset, (MarcSet, AuthSet))
+    assert authset.count == 1
+    assert isinstance(authset.records, map)
+    authset.cache()
+    assert isinstance(authset.records, list)
+    
+    query = QueryDocument(
+        Condition('245', modifier='exists')
         
-    def test_from_query(self):
-        bibset = BibSet.from_query({'_id': {'$in': [555,999]}})
-        self.assertEqual(len(list(bibset.records)),2)
+    )    
+    bibset = BibSet.from_query(query)
+    assert isinstance(bibset, BibSet)
+    assert bibset.count == 2
         
-        query = QueryDocument(
-            Condition(tag='245',subfields={'a': 'Another'})
-        )
-        bibset = BibSet.from_query(query)
-        self.assertEqual(len(list(bibset.records)),1)
+def test_from_table(db):
+    from dlx.marc import BibSet
+    from dlx.util import Table
+    
+    t = Table([
+        ['246a',  '1.246$b',  '1.269c',    '2.269c'],
+        ['title', 'subtitle', '1999-12-31','repeated'],
+        ['title2','subtitle2','2000-01-01','repeated'],
+    ])
+    
+    bibset = BibSet.from_table(t)
+    for bib in bibset.records:
+        assert bib.get_value('246','b')[:8] == 'subtitle'
+        assert bib.get_values('269','c')[1] == 'repeated'
         
-    def test_count(self):
-        query = QueryDocument(Condition('245',modifier='exists'))
-        self.assertEqual(BibSet.from_query(query).count,2)
+    with pytest.raises(Exception):
+        bibset = BibSet.from_table(Table([['245a'], ['This']]), field_check='245a')
         
-    def test_cache(self):
-        query = QueryDocument(Condition('245',{'a': 'Another'}))
-        bibset = BibSet.from_query(query).cache()
-        self.assertEqual(len(list(bibset.records)),1)
-        self.assertEqual(len(list(bibset.records)),1)
+    with pytest.raises(Exception):
+        bibset = BibSet.from_table(Table([['650a'], ['Should an int']]), auth_control=True)
         
-    def test_from_table(self):
-        from dlx.util import Table
+    with pytest.raises(Exception):
+        bibset = BibSet.from_table(Table([['650a'], ['Invalid']]), auth_control=False, auth_flag=True)
         
-        t = Table(
-            [
-                ['246a','1.246$b','1.269c','2.269c'],
-                ['title','subtitle','1999-12-31','repeated'],
-                ['title2','subtitle2','2000-01-01','repeated'],
-            ]
-        )
+def test_from_excel():
+    from dlx.marc import BibSet
+    
+    path = os.path.join(os.path.dirname(__file__), 'marc.xlsx')        
+    bibset = BibSet.from_excel(path, date_format='%Y-%m-%d')
         
-        bibset = BibSet.from_table(t)
-        for bib in bibset.records:
-            self.assertEqual(bib.get_value('246','b')[:8],'subtitle')
-            self.assertEqual(bib.get_values('269','c')[1],'repeated')
-            
-    def test_from_excel(self):
-        path = os.path.join(os.path.dirname(__file__), 'marc.xlsx')        
-
-        bibset = BibSet.from_excel(path, date_format='%Y-%m-%d')
-        for bib in bibset.records:
-            self.assertEqual(bib.get_value('246','b')[:8],'subtitle')
-            self.assertEqual(bib.get_values('269','c')[1],'repeated')
-
-class BatchSerialization(TestCase):
-    def test_xml(self):
-        bibset = BibSet.from_query({'_id': {'$in': [555,999]}})
-        Auth(Data.jauth).commit()
-        Auth(Data.jauth2).commit()
+    for bib in bibset.records:
+        assert bib.get_value('246','b')[:8] == 'subtitle'
+        assert bib.get_values('269','c')[1] == 'repeated'
         
-        self.assertEqual(bibset.to_xml(),'<collection><record><controlfield tag="000">leader</controlfield><controlfield tag="008">controlfield</controlfield><datafield ind1=" " ind2=" " tag="245"><subfield code="a">This</subfield><subfield code="b">is the</subfield><subfield code="c">title</subfield></datafield><datafield ind1=" " ind2=" " tag="520"><subfield code="a">description</subfield></datafield><datafield ind1=" " ind2=" " tag="520"><subfield code="a">another description</subfield><subfield code="a">repeated subfield</subfield></datafield><datafield ind1=" " ind2=" " tag="650"><subfield code="a">header text</subfield></datafield><datafield ind1=" " ind2=" " tag="710"><subfield code="a">another header</subfield></datafield></record><record><controlfield tag="000">leader</controlfield><datafield ind1=" " ind2=" " tag="245"><subfield code="a">Another</subfield><subfield code="b">is the</subfield><subfield code="c">title</subfield></datafield><datafield ind1=" " ind2=" " tag="650"><subfield code="a">header text</subfield></datafield></record></collection>')
-        
+def test_to_mrc(db):
+    from dlx.marc import BibSet
+    
+    control = '00224r|||a2200097|||4500008001300000245002400013520001600037520004300053650001100096710001900107controlfield  aThisbis thectitle  aDescription  aAnother descriptionaRepeated subfield  aHeader  aAnother header00088r|||a2200049|||4500245002700000650001100027  aAnotherbis thectitle  aHeader'
+    
+    assert BibSet.from_query({}).to_mrc() == control
+    
+def test_to_xml(db):
+    from dlx.marc import BibSet
+    
+    control = '<collection><record><controlfield tag="000">leader</controlfield><controlfield tag="008">controlfield</controlfield><datafield ind1=" " ind2=" " tag="245"><subfield code="a">This</subfield><subfield code="b">is the</subfield><subfield code="c">title</subfield></datafield><datafield ind1=" " ind2=" " tag="520"><subfield code="a">Description</subfield></datafield><datafield ind1=" " ind2=" " tag="520"><subfield code="a">Another description</subfield><subfield code="a">Repeated subfield</subfield></datafield><datafield ind1=" " ind2=" " tag="650"><subfield code="a">Header</subfield></datafield><datafield ind1=" " ind2=" " tag="710"><subfield code="a">Another header</subfield></datafield></record><record><controlfield tag="000">leader</controlfield><datafield ind1=" " ind2=" " tag="245"><subfield code="a">Another</subfield><subfield code="b">is the</subfield><subfield code="c">title</subfield></datafield><datafield ind1=" " ind2=" " tag="650"><subfield code="a">Header</subfield></datafield></record></collection>'
+    
+    assert BibSet.from_query({}).to_xml() == control
+    
