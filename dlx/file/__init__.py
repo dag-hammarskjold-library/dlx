7768e60762794710e949dffef8eb56828623379a
jbukhari <joe.bukhari@gmail.com>
2020-05-12 09:21:44 -0400

    use iso util class

--- dlx/file/__init__.py
+++ dlx/file/__init__.py
@@ -1,5 +1,3 @@
-
-
 import os, requests, hashlib
 from datetime import datetime, timezone
 from io import BytesIO
@@ -7,11 +5,9 @@ from json import dumps
 from tempfile import TemporaryFile, SpooledTemporaryFile
 from bson import SON
 from dlx import Config, DB
+from dlx.util import ISO6391
 from dlx.file.s3 import S3
 
-LANG_ISO_CODES = ("aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az", "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs", "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv", "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo", "es", "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga", "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj", "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw", "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv", "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my", "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv", "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps", "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd", "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug", "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi", "yo", "za", "zh", "zu")
-
-###
 
 class FileExists(Exception):
     pass
@@ -120,7 +116,7 @@ class File(object):
         for lang in languages:
             lang = lang.upper()
             
-            if lang.lower() not in LANG_ISO_CODES:
+            if lang.lower() not in ISO6391.codes:
                 raise ValueError('Invalid ISO 639-1 language code')
         
         ###
