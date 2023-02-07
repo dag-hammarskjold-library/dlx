61490aa9e0fa39c4942c500f8b7abf38465643b0
jbukhari <joe.bukhari@gmail.com>
2023-02-07 13:26:45 -0500

    handle more types of connections (#201)
    * handle more types of connections
    
    * bump version and minumum python requirement
    
    * test on 3.11
    
    * use tlsCAFile option for SSL

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-version = '1.2.8.2'
+version = '1.2.9'
 
 import sys
 from setuptools import setup, find_packages
@@ -23,7 +23,7 @@ setup(
     package_data = {'dlx': ['schemas/jmarc.schema.json', 'schemas/jfile.schema.json']},
     test_suite = 'tests',
     install_requires = requirements,
-    python_requires = '>=3.6',
+    python_requires = '>=3.7',
     entry_points = {
         'console_scripts': [
             'excel-marc=dlx.scripts.excel_marc:run',
--- tests/test_db.py
+++ tests/test_db.py
@@ -8,19 +8,21 @@ from dlx import DB
 
 class Test(TestCase):
     def test_connect(self):
-        db = DB.connect('mongodb://u:pw@dummy.com/?authSource=dummy',mock=True)
-        self.assertIsInstance(db,mongomock.MongoClient)
-        self.assertTrue(DB.check_connection())
+        DB.connect('mongodb://u:pw@dummy.com/?authSource=dummy', mock=True)
+        self.assertIsInstance(DB.client, mongomock.MongoClient)
+        assert DB.database_name == 'dummy'
         
-        db = DB.connect('mongomock://localhost')
-        self.assertIsInstance(db,mongomock.MongoClient)
-        self.assertTrue(DB.check_connection())
+        DB.connect('mongomock://localhost')
+        self.assertIsInstance(DB.client, mongomock.MongoClient)
+        assert DB.database_name == 'testing'
+
+        # new connection option
+        DB.connect('mongomock://localhost', database='new_mock_database')
+        assert DB.database_name == 'new_mock_database'
         
     def test_db(self):
-        self.assertIsInstance(DB.handle,mongomock.Database)
-        self.assertIsInstance(DB.bibs,mongomock.Collection)
-        self.assertIsInstance(DB.auths,mongomock.Collection)
-        self.assertIsInstance(DB.files,mongomock.Collection)
-        self.assertIsInstance(DB.config,dict)
-
-    
+        self.assertIsInstance(DB.handle, mongomock.Database)
+        self.assertIsInstance(DB.bibs, mongomock.Collection)
+        self.assertIsInstance(DB.auths, mongomock.Collection)
+        self.assertIsInstance(DB.files, mongomock.Collection)
+        self.assertIsInstance(DB.config, dict)
