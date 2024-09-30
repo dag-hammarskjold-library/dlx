0b897089540b147756f614c96392a31c68f2bd7a
jbukhari <jbukhari@gmail.com>
2024-09-30 16:32:58 -0400

    to_csv double quote bugfix

--- tests/test_marcset.py
+++ tests/test_marcset.py
@@ -170,6 +170,13 @@ def test_to_csv(db):
     #[bib.set('001', None, str(bib.id)) for bib in bibset.records]
     assert bibset.to_csv(write_id=True) == '1.001,1.245$a,1.245$b,1.245$c,1.520$a,2.520$a,1.650$a,1.710$a\n1,This,is the,title,Description,Repeated subfield,Header,Another header\n2,Another,is the,title,,,Header,'
 
+    # comma and quote handling
+    bibs = BibSet()
+    bibs.records += [Bib().set('245', 'a', 'A title, with a comma').set('245', 'b', 'subtitle'), Bib().set('245', 'a', 'A "title, or name" with double quotes in the middle').set('245', 'b', 'subtitle')]
+    print(bibs.to_csv(write_id=False))
+
+    assert bibs.to_csv(write_id=False) == '1.245$a,1.245$b\n"A title, with a comma",subtitle\n"A ""title, or name"" with double quotes in the middle",subtitle'
+
 def test_from_aggregation(db, bibs):
     from dlx.marc import BibSet, Query
 
