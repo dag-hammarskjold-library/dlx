47583f23a856df05600052b62fea04333d945fc6
Aaron Helton <helton@un.org>
2024-11-08 16:27:47 -0500

    Add xref as subfield 0 for CSV export (#426)
    * try testing csv export
    
    * don't alter underlying marc object in csv serialization; pass tests

--- dlx/marc/__init__.py
+++ dlx/marc/__init__.py
@@ -354,8 +354,16 @@ class MarcSet():
                 for place, field in enumerate(record.get_fields(tag)):
                     place += 1
 
+                    xref = None
+
                     for subfield in field.subfields:
                         table.set(i, f'{place}.{field.tag}${subfield.code}', subfield.value)
+                        
+                        if hasattr(subfield, 'xref'):
+                            xref = subfield.xref
+                    
+                    if xref:
+                        table.set(i, f'{place}.{field.tag}$0', str(xref))
 
         # sort the table header
         table.header = MarcSet.sort_table_header(table.header)
